d2aaef3ccdec05a831908d7b18bf9829cce33568
emoacht <emotom@pobox.com>
2014-10-01 17:04:23 +0900

    Updated library and readme

--- SnowyImageCopy/Properties/AssemblyInfo.cs
+++ SnowyImageCopy/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.25")]
-[assembly: AssemblyFileVersion("0.9.6.25")]
+[assembly: AssemblyVersion("0.9.6.26")]
+[assembly: AssemblyFileVersion("0.9.6.26")]
 
