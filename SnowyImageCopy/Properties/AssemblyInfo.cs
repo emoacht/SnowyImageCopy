eed15749518bdd44631b39077dce1cd8022b733a
emoacht <emotom@pobox.com>
2014-10-12 12:31:09 +0900

    Changed minor version

--- SnowyImageCopy/Properties/AssemblyInfo.cs
+++ SnowyImageCopy/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.29")]
-[assembly: AssemblyFileVersion("0.9.6.29")]
+[assembly: AssemblyVersion("0.9.7.29")]
+[assembly: AssemblyFileVersion("0.9.7.29")]
 
