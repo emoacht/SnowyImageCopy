89e9a78a1c0e215a87a98e3221f7739ab7d7b252
emoacht <emotom@pobox.com>
2014-10-13 21:43:13 +0900

    Refactoring

--- SnowyImageCopy/Helper/BitmapExtension.cs
+++ SnowyImageCopy/Helper/BitmapExtension.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 
 namespace SnowyImageCopy.Helper
 {
 	public static class BitmapExtension
 	{
+		#region Win32
+
+		[DllImport("Gdi32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool DeleteObject(IntPtr hObject);
+
+		#endregion
+
+
 		/// <summary>
 		/// Convert a Bitmap to a BitmapImage.
 		/// </summary>
@@ -33,6 +45,32 @@ namespace SnowyImageCopy.Helper
 				image.Freeze();
 
 				return image;
+			}
+		}
+
+		/// <summary>
+		/// Convert a Bitmap to a BitmapSource.
+		/// </summary>
+		/// <param name="source">Source System.Drawing.Bitmap</param>
+		/// <returns>Outcome System.Windows.Media.Imaging.BitmapSource</returns>
+		public static BitmapSource ToBitmapSource(this System.Drawing.Bitmap source)
+		{
+			if (source == null)
+				throw new ArgumentNullException("source");
+
+			var handleBitmap = source.GetHbitmap();
+
+			try
+			{
+				return Imaging.CreateBitmapSourceFromHBitmap(
+					handleBitmap,
+					IntPtr.Zero,
+					Int32Rect.Empty,
+					BitmapSizeOptions.FromEmptyOptions());
+			}
+			finally
+			{
+				DeleteObject(handleBitmap);
 			}
 		}
 	}
