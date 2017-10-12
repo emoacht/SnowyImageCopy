133902492d8539610d4f941a2b6dfb3cbb0a0d98
emoacht <emotom@pobox.com>
2017-10-13 08:01:35 +0900

    Incremented build number

--- Source/SnowyImageCopy/Properties/AssemblyInfo.cs
+++ Source/SnowyImageCopy/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Snowy")]
-[assembly: AssemblyCopyright("Copyright © 2014-2016 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2014-2017 EMO")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,8 +52,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.15")]
-[assembly: AssemblyFileVersion("1.2.2.15")]
+[assembly: AssemblyVersion("1.2.3.16")]
+[assembly: AssemblyFileVersion("1.2.3.16")]
 
 // For unit test
 [assembly: InternalsVisibleTo("SnowyImageCopy.Test")]
