108c7a2b58f8c98949e5ac1613d81e90aa145936
emoacht <emotom@pobox.com>
2020-01-13 12:45:14 +0900

    Increment minor version

--- Source/SnowyImageCopy.Test/Properties/AssemblyInfo.cs
+++ Source/SnowyImageCopy.Test/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SnowyImageCopy.Test")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2015-2020 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("fbfe3c9c-8d09-4abf-82c6-c9b6d0b27aeb")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.2")]
+[assembly: AssemblyFileVersion("1.6.0.2")]
