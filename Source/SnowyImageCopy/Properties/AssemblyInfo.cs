076bb2a936079c761d6ca2ce21468a900e534e42
emoacht <emotom@pobox.com>
2021-05-23 07:59:24 +0900

    Increment minor version

--- Source/SnowyImageCopy/Properties/AssemblyInfo.cs
+++ Source/SnowyImageCopy/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Snowy")]
-[assembly: AssemblyCopyright("Copyright © 2014-2020 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2014 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,8 +52,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.4.0")]
-[assembly: AssemblyFileVersion("2.2.4.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
 
 // For unit test
 [assembly: InternalsVisibleTo("SnowyImageCopy.Test")]
