37866644d763dea44ca984e68d3d95a71279890f
emoacht <emotom@pobox.com>
2021-05-23 07:39:53 +0900

    Modify to accept month-day-year format

--- Source/SnowyImageCopy/Models/Settings.cs
+++ Source/SnowyImageCopy/Models/Settings.cs
@@ -328,7 +328,7 @@ namespace SnowyImageCopy.Models
 				{
 					CustomDatedFolder = null;
 				}
-				else if (IsDatedFolderValid(buffer))
+				else if (IsValidDatedFolder(ref buffer))
 				{
 					CustomDatedFolder = buffer;
 				}
@@ -348,14 +348,20 @@ namespace SnowyImageCopy.Models
 
 		private const string DefaultDatedFolder = "yyyyMMdd";
 
-		private static bool IsDatedFolderValid(string format)
+		private static bool IsValidDatedFolder(ref string format)
 		{
-			// year:  y{1,4}
-			// month: M{1,4}
-			// day:   d{1,2}
-			// delimiter: [-_]?
-			return new Regex("^(?:y{1,4}[-_]?M{1,4}(?:|[-_]?d{1,2})|(?:|d{1,2}[-_]?)M{1,4}[-_]?y{1,4})$")
-				.IsMatch(format ?? string.Empty);
+			// Year:  y{1,4}
+			// Month: M{1,4}
+			// Day:   d{1,2}
+			// Delimiter: [-_]?
+
+			if (string.IsNullOrWhiteSpace(format))
+				return false;
+
+			format = format.Trim().Replace('Y', 'y').Replace('m', 'M').Replace('D', 'd');
+
+			return new Regex("^(?:(?<YMD>y{1,4}[-_]?M{1,4}(?:|[-_]?d{1,2}))|(?<DMY>(?:|d{1,2}[-_]?)M{1,4}[-_]?y{1,4})|(?<MDY>M{1,4}[-_]?d{1,2}[-_]?y{1,4}))$")
+				.IsMatch(format);
 		}
 
 		#endregion
