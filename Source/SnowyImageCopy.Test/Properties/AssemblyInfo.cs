58ff6b1031938e6b41191c6862c8704d21a1a92c
emoacht <emotom@pobox.com>
2022-01-20 14:41:24 +0900

    Increment major version

--- Source/SnowyImageCopy.Test/Properties/AssemblyInfo.cs
+++ Source/SnowyImageCopy.Test/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("fbfe3c9c-8d09-4abf-82c6-c9b6d0b27aeb")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0.0")]
-[assembly: AssemblyFileVersion("2.4.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
