e45aacd59e2ae8633b23e65f523b86ae9c8b0f84
emoacht <emotom@pobox.com>
2022-08-24 17:03:38 +0900

    Refactor Bitmap and BitmapImage extension methods

--- Source/SnowyImageCopy.Shared/Helper/BitmapImageExtension.cs
+++ Source/SnowyImageCopy.Shared/Helper/BitmapImageExtension.cs
@@ -24,15 +24,14 @@ namespace SnowyImageCopy.Helper
 			if (source is null)
 				throw new ArgumentNullException(nameof(source));
 
-			using (var ms = new MemoryStream())
-			{
-				var encoder = new TEncoder();
-				encoder.Frames.Add(BitmapFrame.Create(source));
-				encoder.Save(ms);
-				ms.Seek(0, SeekOrigin.Begin);
+			using var ms = new MemoryStream();
 
-				return new System.Drawing.Bitmap(System.Drawing.Bitmap.FromStream(ms));
-			}
+			var encoder = new TEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(source));
+			encoder.Save(ms);
+			ms.Seek(0, SeekOrigin.Begin);
+
+			return new System.Drawing.Bitmap(ms);
 		}
 
 		/// <summary>
@@ -49,35 +48,33 @@ namespace SnowyImageCopy.Helper
 			if (string.IsNullOrEmpty(filePath))
 				throw new ArgumentNullException(nameof(filePath));
 
-			using (var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
-			{
-				var encoder = new TEncoder();
-				encoder.Frames.Add(BitmapFrame.Create(source));
-				encoder.Save(fs);
-			}
+			using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+			var encoder = new TEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(source));
+			encoder.Save(fs);
 		}
 
 		/// <summary>
-		/// Gets codec info from a BitmapImage.
+		/// Gets codec information from a BitmapImage.
 		/// </summary>
 		/// <typeparam name="TEncoder">BitmapEncoder</typeparam>
 		/// <param name="source">BitmapImage</param>
-		/// <returns>Codec info</returns>
+		/// <returns>Codec information</returns>
 		public static BitmapCodecInfo GetCodecInfo<TEncoder>(this BitmapImage source) where TEncoder : BitmapEncoder, new()
 		{
 			if (source is null)
 				throw new ArgumentNullException(nameof(source));
 
-			using (var ms = new MemoryStream())
-			{
-				var encoder = new TEncoder();
-				encoder.Frames.Add(BitmapFrame.Create(source));
-				encoder.Save(ms);
-				ms.Seek(0, SeekOrigin.Begin);
+			using var ms = new MemoryStream();
 
-				var decoder = BitmapDecoder.Create(ms, BitmapCreateOptions.None, BitmapCacheOption.Default);
-				return decoder.CodecInfo;
-			}
+			var encoder = new TEncoder();
+			encoder.Frames.Add(BitmapFrame.Create(source));
+			encoder.Save(ms);
+			ms.Seek(0, SeekOrigin.Begin);
+
+			var decoder = BitmapDecoder.Create(ms, BitmapCreateOptions.None, BitmapCacheOption.Default);
+			return decoder.CodecInfo;
 		}
 	}
 }
